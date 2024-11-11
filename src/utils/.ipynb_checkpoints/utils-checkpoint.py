8d4ab441aa353b2eb1cfb2a67802d4ff90094f7d
jadetherras <62161504+jadetherras@users.noreply.github.com>
2024-11-11 13:34:26 +0100

    Delete src/utils/.ipynb_checkpoints directory

--- src/utils/.ipynb_checkpoints/utils-checkpoint.py
+++
@@ -1,8 +0,0 @@
-def movie_information(Moviedata, PlotData,ID):
-    """
-    print info on a movie
-    """
-    print("Movie\n") 
-    display(MovieData.loc[ID])
-    print("\nPlot")
-    display(PlotData.loc[ID]["Plot"])
