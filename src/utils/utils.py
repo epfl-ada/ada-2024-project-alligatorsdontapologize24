3228c6167a7f2a09e6360465d663dcdf3da6f32d
Jade Therras <jade.therras@epfl.ch>
2024-11-15 17:12:56 +0100

    pouf

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -6,3 +6,14 @@ def movie_information(Moviedata, PlotData,ID):
     display(MovieData.loc[ID])
     print("\nPlot")
     display(PlotData.loc[ID]["Plot"])
+
+
+def categorize_value(x):
+    if x < -1/3:
+        return -1
+    elif -1/3 <= x <= 1/3:
+        return 0
+    else:
+        return 1
+
+choices = [-1.0, 0.0, 1.0]
