b85d63dde0f7da9d3f6d7d047fd7571764915a4a
Jade Therras <jade.therras@epfl.ch>
2024-12-05 13:55:19 +0100

    binary LLMé

---
+++ src/model/OpenIA_utility_binary.py
@@ -0,0 +1,126 @@
+"""
+GPT4mini custom model with utility functions
+--------------------------------------
+This script regroup all the utility functions for the LLM, and implement the model.
+"""
+
+import numpy as np
+import pandas as pd
+import os
+import tiktoken
+import openai
+from openai import OpenAI
+import json
+
+
+class GPT4mini_ViolenceClassifier_extreme:
+    def __init__(self):
+        #API parameters
+        #key (toc toc)
+        openai.api_key = os.environ["OPENAI_API_KEY"]
+        #
+        self.client = OpenAI()
+
+        #model parameters
+        self.max_input = 128000 #maximum imput tokens for the model
+        #self.max_input = 8000 #maximum imput tokens for the model
+        self.pricing = 1/1000000*0.150 #price per token
+        self.encoder = tiktoken.encoding_for_model("gpt-4o-mini") #for gpt-4o-mini
+        
+        #prompt ingenieurring
+        self.Label = """
+        - **Non-violent**: The text describes no extreme physical or psychological violence. There are no recurrent aggression, conflict, or harm to people or animals.
+        - **Violent**: The text describe extreme physical or psychological violence, such as physical aggression, conflict, or harm. Scenes may include fighting, injury, rape. It is a prominent feature of the film."""   
+        self.Instruction = "Classify each plot between violent and non-violent. Respond with a dictionary where the keys are the plot numbers (e.g., 'plot1', 'plot2') and the values are the levels of violence ('Non-violent', 'Violent')"
+        
+        self.Example = """Here are some examples for each label :
+        - **Non-violent**: plot1 :'norma and malcolm miochaels are a middle-aged married couple who are in the midst of a midlife crisis. both decide to separate and begin their lives anew away from each other. however, problems ensue once they discover that they are no longer as young as they used to be.'
+        plot2:'in the 1840s, two sisters fall in love with the same man. while drunk, the man writes a letter proposing marriage to the wrong one.'
+        - **Violent**: plot1:'Richard Beck  is a police detective who believed that rape victims are to blame for the crime. He is later raped by two of the suspects he had been chasing. Ultimately, he changes his beliefs about rape victims. This made for TV movie was groundbreaking in that it portrayed the rape of a man by two other men, and because of this it has become a cult classic.'
+        plot2:'newlywed carl  goes to war where he endures major suffering. back home, wife pauli  starves, becomes a prostitute to survive, and their baby dies.'
+        """
+        
+        self.Content = f"### Violence scale : ###{self.Label}\n\n### Instructions ###\n{self.Instruction}"
+
+        self.Prompt_size = self.count_tokens(self.Content) + self.count_tokens(self.Example)
+        #function
+        self.function = {
+           "name": "Assign_violence_level",
+           "description": "Binary classification of the level of violence of a list of movie plots",
+           "parameters": {
+               "type": "object",
+               "properties": {
+                   "prediction": {
+                       "type": "array",
+                       "items": {
+                           "type": "string",
+                           "enum": [
+                               "Non-violent",
+                               "Violent"
+                           ]
+                       },
+                       "description": "The list of violence levels for each movie plot, in the same order as the plots were provided."
+                   }
+               },
+               "required": [
+                   "prediction"
+               ]
+           }
+        }
+    
+    def count_tokens(self,text):
+        return len(self.encoder.encode(text))
+
+    def format_plot(self,i,text):
+        return f"plot{i}:{text}\n\n"
+
+    def format_batch(self,Data,size = 0):
+        Text = ""
+        for i in range(0,Data.shape[0]) :
+            Text += self.format_plot(i+1,Data.iloc[i]['Plot'])
+        #last check
+        size = self.count_tokens(Text)
+        if size > (self.max_input-self.Prompt_size) :
+            print("size : {}, max size {}".format(size + self.Prompt_size,self.max_input))
+            raise Exception("ho ho.. this is too big")
+        
+        return Text
+
+    def batch_plots(self,Data,size = 0):
+        if size == 0:
+            size = self.max_input
+        print("size",self.Prompt_size)
+        batch = [0]
+        currentBatch = self.Prompt_size
+        for i in range(0,Data.shape[0]):
+            plot_tokens = self.count_tokens(self.format_plot(i+1,Data.iloc[i]["Plot"]))
+            if currentBatch + plot_tokens > size:
+                batch.append(i)
+                #print("batch margin:",self.max_input-currentBatch)
+                currentBatch = self.Prompt_size
+            else:
+                currentBatch += plot_tokens
+        print("Final number of batchs",len(batch))
+        return batch
+
+    def Call_API(self,Text):
+        #call the API
+        completion = self.client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": self.Content},
+            {"role": "user","content": Text},
+            {"role": "assistant", "content": self.Example}
+        ],
+        functions=[self.function],
+        function_call={"name": "Assign_violence_level"},
+    )
+        #extract the answer
+        try:
+            prediction = json.loads(completion.choices[0].message.function_call.arguments)["prediction"]
+            return prediction
+            #prediction = completion.choices[0].message
+        except (KeyError, json.JSONDecodeError) as e:
+            print(f"Error extracting prediction: {e}")
+            return completion
+    
