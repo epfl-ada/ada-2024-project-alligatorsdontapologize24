2801fd91ac105c3719b3ec60c3887198fceb1c66
Raphael Hellmann <raphael.hellmann@epfl.ch>
2024-12-05 16:17:49 +0100

    first draft of ARDL model function

--- src/model/OLS_regression.py
+++ src/model/OLS_regression.py
@@ -6,10 +6,14 @@ import statsmodels.api as sm
 import statsmodels.formula.api as smf
 from statsmodels.regression.linear_model import RegressionResultsWrapper
 
+from statsmodels.tsa.ardl import select_order
+
+
+
 def OLS_regression(violence:pd.DataFrame, box_revenues_violent: pd.DataFrame, unemployment_rate: pd.DataFrame) -> RegressionResultsWrapper:
 
     """
-    Creates and returns a statsmodels OLS model.
+    Creates and returns a statsmodels ARDL model.
     
     Parameters:
     - dependent: pandas Series of real-world violence score
@@ -19,13 +23,25 @@ def OLS_regression(violence:pd.DataFrame, box_revenues_violent: pd.DataFrame, un
     - A statsmodels OLS RegressionResultsWrapper object
     """
 
+    # Sum up the box office revenues of all violent films per week
+    weekly_revenues = box_revenues_violent.groupby(["Year", "Week"])["box_office_revenue"].sum()
+
+    # PREPROCESSING OF UNEMPLOYMENT RATE DATAFRAME? -> MUST ALSO BE IN WEEKLY NUMBERS IN THE SAME TIMESPAN
+
+    # PREPROCESSING OF VIOLENCE DATAFRAME? -> MUST ALSO BE IN WEEKLY NUMBERS IN THE SAME TIMESPAN
+
+    # Concatenate the two exogenous variables
+    EXOG = pd.concat([weekly_revenues, weekly_unemployment], axis=1)
+
+
     # ----------------------- Model definition for auto-regressive distributed lag model ----------------- #
 
     # Setting the time frame for the auto-regressive part
-    timespan_violence = 1   # take into account 1 previous timestep
+    max_auto_lag = 4            # take into account max. 4 previous timesteps
 
     # Setting the time span for the distributed lag part
-    timespan_films = 4      # take into account 4 previous timesteps
+    max_film_lag = 4            # take into account max. 4 previous timesteps
+    max_unemployment_lag = 1    # take into account max 1 previous timestep
 
     # Include time-fixed effects
     time_fixed = True
@@ -33,17 +49,22 @@ def OLS_regression(violence:pd.DataFrame, box_revenues_violent: pd.DataFrame, un
     # Include additional confounding factors
     include_confounding = True
 
+    # INCLUDE TIME DUMMY VARIABLES FOR TIME FIXED EFFECTS?
 
-    # ------------------------------------------ Set up the model ---------------------------------------- #
+    # Get indicator variables for the year-week
+    EXOG["Year-Week"] = EXOG["Year"] + "-" + EXOG["Week"]
 
-    # Declare the model
-    model = smf.ols(formula='time ~ C(diabetes) + C(high_blood_pressure)', data=df)
+    # Create time dummies for weekly time-fixed effects
+    time_dummies = pd.get_dummies(EXOG["Year-Week"], drop_first=True)
+    EXOG_with_dummies = pd.concat([EXOG, time_dummies], axis=1)
 
-    # Fit the model
-    np.random.seed(2)
-    model_fitted = model.fit()
+    # --------------------------------------- Set up select_order --------------------------------------- #
 
-    # Print the summary
-    #print(res.summary())
+    # Automatically select lag order based on AIC
+    selected_order = select_order(violence['violence_score'], EXOG_with_dummies, maxlag=max_auto_lag, maxorder={'box_office_revenue':max_film_lag,'unemployment_rate':max_unemployment_lag}, ic='aic')
 
-    return model_fitted
+    # Fit ARDL model with the selected order
+    model = selected_order.model
+    
+
+    return model
