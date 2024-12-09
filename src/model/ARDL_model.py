a51c9324fad6cb56a38160f4099ef7f89f87d3d9
Raphael Hellmann <raphael.hellmann@epfl.ch>
2024-12-09 17:58:31 +0100

    first implementations ARDL

--- src/model/ARDL_model.py
+++ src/model/ARDL_model.py
@@ -6,11 +6,11 @@ import statsmodels.api as sm
 import statsmodels.formula.api as smf
 from statsmodels.regression.linear_model import RegressionResultsWrapper
 
-from statsmodels.tsa.ardl import select_order
+#from statsmodels.tsa.ardl import select_order
 
+# RegressionResultsWrapper
 
-
-def ARDL_model(violence:pd.DataFrame, box_revenues_violent: pd.DataFrame, unemployment_rate: pd.DataFrame) -> RegressionResultsWrapper:
+def ARDL_model_func(box_revenues_violent: pd.DataFrame, violence:pd.DataFrame = None,  unemployment_rate: pd.DataFrame = None) -> pd.DataFrame:
 
     """
     Creates and returns a statsmodels ARDL model.
@@ -23,18 +23,42 @@ def ARDL_model(violence:pd.DataFrame, box_revenues_violent: pd.DataFrame, unempl
     - A statsmodels OLS RegressionResultsWrapper object
     """
 
-    # Sum up the box office revenues of all violent films per week
-    weekly_revenues = box_revenues_violent.groupby(["Year", "Week"])["box_office_revenue"].sum()
+    # ---------------------------------- Preprocess the box_revenues_violent data ------------------------------ #
 
+    # Drop all lines containing NaN
+    box_revenues_clean = box_revenues_violent.dropna()
+
+    # Sum up the box office revenues of all violent films per week
+    weekly_revenues = box_revenues_clean.groupby(["Year", "Week"])["Box office revenue"].sum().reset_index()
+
+    # Count the number of films released per week
+    weekly_no_films = box_revenues_clean.groupby(["Year", "Week"]).size().reset_index(name="no. films released")
+
+    # Merge both in one dataframe
+    weekly_films_revenues = pd.merge(weekly_no_films, weekly_revenues, on=["Year", "Week"], how="left")
+
+    # Sort by year and week (from old to young)
+    weekly_films_revenues_sorted = weekly_films_revenues.sort_values(["Year", "Week"], ascending=False)
+
+
+    # ---------------------------------- Preprocess the unemployment data -------------------------------------- #
+
+    # To Do
     # PREPROCESSING OF UNEMPLOYMENT RATE DATAFRAME? -> MUST ALSO BE IN WEEKLY NUMBERS IN THE SAME TIMESPAN
 
+
+    # ------------------------------------- Preprocess the violence data --------------------------------------- #
+
+    # To Do
     # PREPROCESSING OF VIOLENCE DATAFRAME? -> MUST ALSO BE IN WEEKLY NUMBERS IN THE SAME TIMESPAN
 
-    # Concatenate the two exogenous variables
-    EXOG = pd.concat([weekly_revenues, weekly_unemployment], axis=1)
+    # ---------------------------------------------------------------------------------------------------------- #
+
+    # Concatenate the two exogenous variables (take only values for the time frame under analysis)
+    #EXOG = pd.concat([weekly_revenues, weekly_unemployment], axis=1)
 
 
-    # ----------------------- Model definition for auto-regressive distributed lag model ----------------- #
+    """ # ----------------------- Model definition for auto-regressive distributed lag model ----------------- #
 
     # Setting the time frame for the auto-regressive part
     max_auto_lag = 4            # take into account max. 4 previous timesteps
@@ -64,7 +88,7 @@ def ARDL_model(violence:pd.DataFrame, box_revenues_violent: pd.DataFrame, unempl
     selected_order = select_order(violence['violence_score'], EXOG_with_dummies, maxlag=max_auto_lag, maxorder={'box_office_revenue':max_film_lag,'unemployment_rate':max_unemployment_lag}, ic='aic')
 
     # Fit ARDL model with the selected order
-    model = selected_order.model
+    model = selected_order.model """
     
 
-    return model
+    return weekly_films_revenues_sorted
